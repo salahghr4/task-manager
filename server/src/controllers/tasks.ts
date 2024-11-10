fd2005eb1aea5b650ffbc905a97263d4b803cee0
salahghr4 <salahghr4@gmail.com>
2024-11-10 22:37:46 +0100

    feat: Implement task model & Add Task Creation Functionality

--- server/src/controllers/tasks.ts
+++ server/src/controllers/tasks.ts
@@ -1,11 +1,20 @@
 import { Request, Response } from "express";
+import { TaskModel } from "../models/tasks";
 
 export const getAlltasks = (req: Request, res: Response) => {
   res.status(200).json({ message: "get all tasks" });
 };
 
-export const createTask = (req: Request, res: Response) => {
-  res.status(200).json({ message: "task added", task: req.body });
+export const createTask = async (req: Request, res: Response) => {
+  try {
+    const task = await TaskModel.create(req.body);
+    res.status(201).json({
+      message: "task created successfully",
+      task,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 };
 
 export const getTask = (req: Request, res: Response) => {
---
+++ server/src/models/tasks.ts
@@ -0,0 +1,37 @@
+import { Schema, model, ObjectId, Model } from "mongoose";
+
+interface Itask {
+  _id: ObjectId;
+  title: string;
+  description: string;
+  completed: boolean;
+  important: boolean;
+}
+
+const tasksSchema = new Schema<Itask>(
+  {
+    title: {
+      type: String,
+      required: [true, "must provide a task name"],
+      trim: true,
+      maxlength: [20, "title cannot be more than 20 characters"],
+    },
+    description: {
+      type: String,
+      trim: true,
+      default: "",
+      maxlength: [300, "description cannot be more than 300 characters"],
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    important: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+export const TaskModel: Model<Itask> = model<Itask>("Task", tasksSchema);
