83fdcb70da95830e89cc47420e8abeffa2f566d9
salahghr4 <salahghr4@gmail.com>
2024-11-14 23:21:53 +0100

    chors: add cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import tasksRoutes from "./routes/tasks";
+import cors from "cors";
+import envs from "./core/config/env";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({ origin: envs.CLIENT_URL}));
 
 app.use("/api/v1/tasks", tasksRoutes);
 
--- server/src/core/config/env.ts
+++ server/src/core/config/env.ts
@@ -5,6 +5,7 @@ const envs = {
   PORT: get("PORT").required().asPortNumber(),
   API_PREFIX: get("API_PREFIX").default("/api/v1").asString(),
   MONGODB_URI: get("MONGODB_URI").required().asUrlString(),
+  CLIENT_URL: get("CLIENT_URL").required().asString()
 };
 
 export default envs;
